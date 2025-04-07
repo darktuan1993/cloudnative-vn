c252451b2edede85eac04f904d71ab4bc503a6d9
DarkTuan <bachdangtuan.dev@gmail.com>
2025-04-07 21:20:04 +0700

    update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'TuanBach',
   author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  headerTitle: 'TuanBach',
+  description: 'Trang cái nhân của Bạch Đăng Tuấn',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
