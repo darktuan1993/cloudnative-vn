45c8ca370233ea195703221661d3853bebc48197
DarkTuan <bachdangtuan.dev@gmail.com>
2025-04-10 20:37:04 +0700

    update

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Bài Viết' },
-  // { href: '/tags', title: 'Tags' },
-  // { href: '/projects', title: 'Các Dự Án' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'Thảo Luận' },
   { href: '/about', title: 'Giới Thiệu' },
 ]
 
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,10 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Hướng dẫn thi chứng chỉ Prometheus Certified Associate (PCA)',
+    description: `Hành trình chinh phục chứng chỉ Prometheus không đơn thuần là việc học lý thuyết suông, Bài viết này sẽ chia sẻ quá trình tôi ôn luyện, chiến lược thi, và những mẹo nhỏ để bạn có thể tự tin vượt qua kỳ thi....`,
+    imgSrc: '/static/images/prometheus-cert.png',
+    href: '/blog/cauchuyen/kinh-nghiem-thi-chung-chi-prometheus',
   },
 ]
 
